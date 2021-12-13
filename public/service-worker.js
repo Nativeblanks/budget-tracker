e1777b98064b3268aa12db6023af1d499640281c
Zach B <nativeblanks@gmail.com>
2021-12-12 21:15:45 -0500

    move out of develop folder

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/js/idb.js",
+    "/index.html",
+    "/manifest.json",
+    "/css/style.css",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png"
+];
+
+const APP_PREFIX = "BudgetTracker";
+const VERSION = "version_01";
+const CACHE_NAME = "";
